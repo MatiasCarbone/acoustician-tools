648c0f4494cc58097b987d8b9885e4ab90ce0bd4
matiascarbone <matiascarbone2@gmail.com>
2024-03-27 18:34:44 -0300

    feat: add sabine rt60 calculation

---
+++ acoustician_tools/room.py
@@ -0,0 +1,29 @@
+"""
+ROOM
+
+This module contains functions for calculating theoretical reverberation times of rooms, 
+as well as calculating decay times and clarity from impulse-response files.
+"""
+
+import numpy as np
+
+SOUNDSPEED = 343.0  # m/s @ 20°C dry air
+
+
+def t60_sabine(volume, surfaces, alphas, c=SOUNDSPEED):
+    """
+    Calculate theoretical reverberation time using Sabine's equation.
+
+    Parameters:
+        volume (float): Total volume of the room [m3]
+        surfaces (list of floats): Surface area of each boundary [m2]
+        alphas (list of floats): Absortion coefficient for each boundary [0-1]
+
+    Returns:
+        t60: Reverberation time in seconds. Amount of time required for a decay of 60dB
+    """
+    total_surface = np.sum(surfaces)
+    mean_alpha = np.average(alphas, weights=surfaces)
+
+    t60 = 0.161 * volume / (total_surface * mean_alpha)
+    return t60
