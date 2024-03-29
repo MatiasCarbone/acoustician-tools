2b8f505251c2137ca914d6f28d947d6a409e7681
matiascarbone <matiascarbone2@gmail.com>
2024-03-29 00:12:51 -0300

    Add multiband functionality to Sabine T60

--- acoustician_tools/room.py
+++ acoustician_tools/room.py
@@ -10,21 +10,42 @@ import numpy as np
 SOUNDSPEED = 343.0  # m/s @ 20°C dry air
 
 
-def t60_sabine(volume: float, surfaces: list[float], alphas: list[float], c: float = SOUNDSPEED) -> float:
+def shoebox_surfaces(length: float, width: float, height: float) -> list:
     """
-    Calculate theoretical reverberation time using Sabine's equation.
+    Get list of each boundary's surface for a shoebox room with defined dimensions.
+
+    Parameters:
+        length (float): length of the room [m]
+        width (float): width of the room [m]
+        heigth (float): heigth of the room [m]
+
+    Returns:
+        surfaces (list of floats): list containig the surface of each wall [m2]
+    """
+    sidewalls = length * height
+    front_rear = width * height
+    roof_ceil = width * length
+
+    surfaces = [sidewalls, sidewalls, front_rear, front_rear, roof_ceil, roof_ceil]
+    return surfaces
+
+
+def t60_sabine(volume: float, surfaces: list, alphas: list, c: float = SOUNDSPEED) -> list[float]:
+    """
+    Calculate theoretical reverberation time using Sabine's equation for one or more frequency bands.
 
     Parameters:
         volume (float): Total volume of the room [m3]
         surfaces (list of floats): Surface area of each boundary [m2]
-        alphas (list of floats): Absortion coefficient for each boundary [0-1]
+        alphas (1d or 2d list of floats): Absortion coefficient for each boundary; [0-1]
+            first dimension corresponds to boundary and second dimension to alpha
 
     Returns:
-        t60: Reverberation time in seconds. Amount of time required for a decay of 60dB
+        t60 (float or list): Reverberation time in seconds. Amount of time required for a decay of 60dB
+            for one at one or more frequency bands.
     """
 
     total_surface = np.sum(surfaces)
-    mean_alpha = np.average(alphas, weights=surfaces)
-
+    mean_alpha = np.average(alphas, axis=-1, weights=surfaces)
     t60 = 0.161 * volume / (total_surface * mean_alpha)
     return t60
--- tests/test_reverb.py
+++ tests/test_reverb.py
@@ -3,22 +3,42 @@ import sys
 sys.path.append('../acoustician-tools')
 
 import unittest
-from acoustician_tools.room import t60_sabine
+import numpy as np
+from acoustician_tools.room import t60_sabine, shoebox_surfaces
 
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
-        self.volume = 3000
-        self.surfaces = [240, 600, 500]
-        self.alphas = [0.1, 0.25, 0.45]
+        # Initialize properties here
+        pass
+
+    def test_shoebox_surfaces(self):
+        expected = [8, 8, 6, 6, 12, 12]
+        self.assertEqual(shoebox_surfaces(4, 3, 2), expected)
 
     def test_sabine(self):
+        # Single band
         expected = 1.2113
         self.assertAlmostEqual(
-            t60_sabine(self.volume, self.surfaces, self.alphas),
+            t60_sabine(3000, [240, 600, 500], [0.1, 0.25, 0.45]),
             expected,
             msg=f'Should be close to {expected}.',
             places=2,
+        )
+
+        # Multiple bands
+        l, w, h = 2, 4, 3
+        vol = l * w * h
+        a = [
+            [0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
+            [0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
+        ]
+        expected = np.asarray([0.14861538, 0.74307692])
+        self.assertTrue(
+            np.allclose(
+                t60_sabine(vol, shoebox_surfaces(l, w, h), alphas=a),
+                expected,
+            )
         )
 
 
