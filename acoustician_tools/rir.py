d9724f0c7d3645f4a150b41502c93f511b878f92
Matías Carbone <matiascarbone2@gmail.com>
2024-04-08 04:01:29 -0300

    WIP: add function for RT60 from impulse
    Calculates the RT60 for each band using Schroeder Integration and linear regression for estimating RT60 values.
    
    Can be used with EDT, T10, T20 or T30 estimations and custom frequency bands.

--- acoustician_tools/rir.py
+++ acoustician_tools/rir.py
@@ -5,15 +5,54 @@ This module contains functions for loading and processing room impulse-responses
 and make various acoustical calculations.
 """
 
-import csv
 import numpy as np
+from scipy.io import wavfile
+from scipy.stats import linregress
+from acoustician_tools.filter import butter_bandpass, butter_bandpass_filter
 
 
-def load_ir_mono(path: str, sep: str = ','):
-    """
-    Load a mono IR from CSV or text file.
-    """
-    with open(path) as file:
-        csv_reader = csv.reader(file, delimiter=sep)
-        data = [float(x[0]) for x in csv_reader]
-    return data
+def rt60_from_ir(path: str, bands: list, estimator: str = 't30'):
+    sr, ir_signal = wavfile.read(path)
+    rt = []
+
+    # Get reference decay points based on selected estimator
+    estimator = str.lower(str(estimator))
+    match estimator:
+        case 'edt':
+            drop = (0, -10)
+            multiplier = 6
+        case 't10':
+            drop = (-5, -15)
+            multiplier = 6
+        case 't20':
+            drop = (-5, -25)
+            multiplier = 3
+        case 't30':
+            drop = (-5, -35)
+            multiplier = 2
+        case 't60':
+            pass
+        case _:
+            raise TypeError('Invalid estimator. Only valid options are "edt", "t10", "t20", "t30" and "t60".')
+
+    for b in bands:
+        y = butter_bandpass_filter(ir_signal, b[0], b[1], sr, order=8)  # Bandpassed signal
+        y_abs = np.abs(y) / np.max(np.abs(y))  # Absolute values, normalized
+
+        # Scroeder integration
+        sch = np.cumsum(y_abs[::-1] ** 2)[::-1]  # Backwards integration
+        sch_db = sch_db = 10.0 * np.log10(sch / np.max(sch))  # Converted to dB
+
+        # Reference decay x values points for slicing
+        a = np.where(sch_db <= drop[0])[0][0]
+        b = np.where(sch_db <= drop[1])[0][0]
+
+        sch_db_slice = sch_db[a:b]
+        t = np.linspace(0, (len(sch_db_slice) / sr), len(sch_db_slice))
+        slope, intercept = linregress(t, sch_db_slice)[:2]
+
+        regress_start = (drop[0] - intercept) / slope
+        regress_end = (drop[1] - intercept) / slope
+        rt.append(multiplier * (regress_end - regress_start))
+
+    return rt
--- tests/test_rir.py
+++ tests/test_rir.py
@@ -9,23 +9,11 @@ from acoustician_tools.rir import *
 
 
 class TestRIR(unittest.TestCase):
-    def test_load_ir_mono(self):
-        length = 262144
-        first_10 = [
-            9.687375e-06,
-            -5.1842585e-06,
-            -3.502343e-05,
-            -2.176988e-05,
-            2.126425e-06,
-            -3.8547214e-06,
-            -1.7837274e-05,
-            -5.584359e-06,
-            1.5538884e-06,
-            -3.911781e-06,
-        ]
-        ir = load_ir_mono('tests/IR/IR_test.txt')
-        self.assertEqual(len(ir), length)
-        self.assertEqual(ir[0:10], first_10)
+    # def test_rt60_from_ir(self):
+    #     expected = 0.512
+    #     calculated = rt60_from_ir('tests/IR/IR_test.wav', [(88, 177)], 't30')
+
+    #     self.assertAlmostEqual(*calculated, expected)
 
 
 if __name__ == '__main__':
